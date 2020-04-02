4c59af4caf70df3291914b8b1328f15a3d668a5d
Lukas Hirsch <hirschlukasa@gmail.com>
2020-04-01 21:47:01 -0500

    lots of comments

--- app.js
+++ app.js
@@ -1,23 +1,26 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+//importing classes and libraries
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
 
-const OUTPUT_DIR = path.resolve(__dirname, "output")
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+//gets filepath for output folder where html file will be generated
+const OUTPUT_DIR = path.resolve(__dirname, 'output')
+//generates team html file
+const outputPath = path.join(OUTPUT_DIR, 'team.html');
 
-const render = require("./lib/htmlRenderer");
+
+
+const render = require('./lib/htmlRenderer');
 
 //add emps and manager to office
 //add office to company
 
-let companyStaff = {}
-
 let teamMembers = [];
 
-
+//question array for inquirer
 const questions = [
     {
         type: "input",
@@ -34,10 +37,18 @@ const questions = [
         type: "input",
         message: "Enter id number",
         name: "id",
+        //validation for id number
         validate : answer =>{
+            //first checks against regex to make sure only numeric characters are entered
            const valid = answer.match(
                 `^[0-9]*$` );
                 if (valid){
+                    //if the input passes the regex test, it is checked against the other employee's IDs to make sure that it is unique
+                   for(i=0;i<teamMembers.length;i++){
+                       if(answer === teamMembers[i].id){
+                           return 'Please enter a unique id'
+                       }
+                   }
                     return true
                 }
                 return 'Please enter a number'
@@ -48,6 +59,7 @@ const questions = [
         message: 'Enter email',
         name: 'email',
         validate: answer => {
+            //validation checks against regex to make sure email is formatted correctly. string @ string . string
             const valid = answer.match(
               /\S+@\S+\.\S+/
             );
@@ -74,17 +86,21 @@ const questions = [
         }
     }
 ]
-//add team member​
-//if statement if add another is true call addTeamMember again else don't
-function buildTeam(){
-    //which branch would you like to view?
+
+//start function is ran on load
+function start(){
+    //asks user which branch they would like to edit
     inquirer.prompt({
+        //first question asks branch
         type: 'list',
         message : 'Which branch would you like to edit?',
         choices : ['New York', 'Seattle', 'Minneapolis'],
         name : 'branch'
     }).then(function(data){
+        //gets branch name from previous question
         var branchName = data.branch;
+        //asks if user would like to view or edit current team. 
+        //as of right now, only edit is functional. view will be updated once the database connection is made. 
         inquirer.prompt({
             type : 'list',
             message: `Would you like to view or edit the team members of the ${branchName} branch?`,
@@ -92,9 +108,11 @@ function buildTeam(){
             name : 'editChoice'
         }).then(function(data){
             if(data.editChoice === 'View'){
-                console.log('view');
+                //view functionality will be updated once database integration is complete
+                console.log('view functionality will be updated once database integration is complete');
             }
             else{
+                //calls function to add manager
                 addManager(branchName);
             }
         })
@@ -125,13 +143,15 @@ function addManager(branch){
         const manager = new Manager(data.name, data.id, data.email, branch)
         //pushes new manager object into team member array
         teamMembers.push(manager);
+        //calls function
         newMemberPrompt();
     })
 
 }
 
+//this function asks the user if they would like to add another team member
 function newMemberPrompt(){
-
+    //user prompts
     inquirer.prompt(
         {
             type : 'list',
@@ -140,35 +160,33 @@ function newMemberPrompt(){
             name : 'addMember'
         }
     ).then(function(data){
+        //adds new member
         if (data.addMember === 'Yes'){
             addTeamMember();
         }
+        //if the user says that they do not want to add another team member, renderTeam function is called
         else{
-            console.log('final team ' ,teamMembers)
             renderTeam();
         }
     })
 }
 
+//this function is used to create engineers and interns
 function addTeamMember(){
+    //questions array is passed into inquirer
     inquirer.prompt(questions).then(function (data) {
-
         //switch case is based on employee's role
         //creates appropriate object and adds object to the teamMembers array
         //finally, it calls the function to prompt for a new team member
         switch (data.role) {
             case 'Engineer':
                 const engineer = new Engineer(data.name, data.id, data.email, data.github);
-                console.log("i'm an engineer ", engineer)
                 teamMembers.push(engineer);
-                console.log('current team members : ', teamMembers)
                 newMemberPrompt();
                 break;
-            case "Intern":
+            case 'Intern':
                 const intern = new Intern(data.name, data.id, data.email, data.school);
-                console.log("i'm an intern ", intern)
                 teamMembers.push(intern);
-                console.log('current team members : ', teamMembers)
                 newMemberPrompt();
                 break;
     
@@ -178,29 +196,10 @@ function addTeamMember(){
     })
 }
 
+//renderteam is called and html is generated
 function renderTeam(){
-    fs.writeFileSync(outputPath, render(teamMembers), "utf-8")
+    fs.writeFileSync(outputPath, render(teamMembers), 'utf-8');
+    console.log('output dir :',OUTPUT_DIR)
 };
-buildTeam();
-// // Write code to use inquirer to gather information about the development team members,
-// // and to create objects for each team member (using the correct classes as blueprints!)
-// ​
-// // After the user has input all employees desired, call the `render` function (required
-// // above) and pass in an array containing all employee objects; the `render` function will
-// // generate and return a block of HTML including templated divs for each employee!
-// ​
-// // After you have your html, you're now ready to create an HTML file using the HTML
-// // returned from the `render` function. Now write it to a file named `team.html` in the
-// // `output` folder. You can use the variable `outputPath` above target this location.
-// // Hint: you may need to check if the `output` folder exists and create it if it
-// // does not.
-// ​
-// // HINT: each employee type (manager, engineer, or intern) has slightly different
-// // information; write your code to ask different questions via inquirer depending on
-// // employee type.
-// ​
-// // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// // and Intern classes should all extend from a class named Employee; see the directions
-// // for further information. Be sure to test out each class and verify it generates an 
-// // object with the correct structure and methods. This structure will be crucial in order
-// // for the provided `render` function to work!```
+start();
+
